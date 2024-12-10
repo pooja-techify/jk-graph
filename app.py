b570d85123f903c647654d3b6082c4b61fead92a
pooja-techify <pooja.shah@techifysolutions.com>
2024-12-10 15:08:50 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -977,12 +977,16 @@ def excel_chase():
                 response.tables[i].visualize()
                 table_title = table[0].title
                 if table_title:
+                    # print(table_title.text)
                     if table_title.text.startswith('DEPOSIT'):
                         df=table[0].to_pandas()
+                        # print(df)
                         if len(df.columns) > 3:
                             for i in range(2, len(df.columns)-1):
                                 df[1] = df[1] + ' ' + df[i]
-                        df1 = df[[0,1,len(df.columns)-1]].rename(columns={0: "date", 1: "description", len(df.columns)-1: "amount"})
+                        df1 = df[[0,1,len(df.columns)-1]].copy()
+                        df1 = df1.rename(columns={0: "date", 1: "description", len(df.columns)-1: "amount"})
+                        # print(df1)
                         credits_aws = pd.concat([credits_aws, df1], ignore_index=True)
 
                     if table_title.text in ['ATM & DEBIT CARD WITHDRAWALS', 'ELECTRONIC WITHDRAWALS', 'FEES']:
@@ -991,7 +995,9 @@ def excel_chase():
                         if len(df.columns) > 3:
                             for i in range(2, len(df.columns)-1):
                                 df[1] = df[1] + ' ' + df[i]
-                        df1 = df[[0,1,len(df.columns)-1]].rename(columns={0: "date", 1: "description", len(df.columns)-1: "amount"})
+                        df1 = df[[0,1,len(df.columns)-1]].copy()
+                        df1 = df1.rename(columns={0: "date", 1: "description", len(df.columns)-1: "amount"})
+                        # print(df1)
                         debits_aws = pd.concat([debits_aws, df1], ignore_index=True)
 
         debits_aws = debits_aws[debits_aws.iloc[:,0].str.match(r'^\d{2}/\d{2}', na=False)].reset_index(drop=True)
