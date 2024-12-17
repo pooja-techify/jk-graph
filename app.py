f08300d39fa4ebdec6dafd4d84050f4eb64c09a5
pooja-techify <pooja.shah@techifysolutions.com>
2024-12-17 11:51:50 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -338,12 +338,13 @@ def excel_bcb():
                 if table_title:
                     if table_title.text in ["ACTIVITY DESCRIPTION"]:
                         df=table[0].to_pandas()
-                        for i in range(2, len(df)):
-                            val = df.iloc[i][0]
-                            if not re.match(r'^\d{1,2}/\d{2}', val):
-                                for j in range(len(df.columns)):
-                                    df.loc[i-1, 0] += ' ' + df.iloc[i][j]
-                                df.iloc[i,0].strip()
+                        if len(df) > 2:
+                            for i in range(2, len(df)):
+                                val = df.iloc[i][0]
+                                if not re.match(r'^\d{1,2}/\d{2}', val):
+                                    for j in range(len(df.columns)):
+                                        df.loc[i-1, 0] += ' ' + df.iloc[i][j]
+                                    df.iloc[i,0].strip()
                         transactions = pd.concat([transactions, df], ignore_index=True)
 
         
